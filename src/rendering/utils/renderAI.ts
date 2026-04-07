451daa2268a84395592835aceb25a13afce16f68
megaofmegalodon <testtrolium@gmail.com>
2026-04-07 16:13:19 -0400

    chore: remove unnecessary comments

--- src/rendering/utils/renderAI.ts
+++ src/rendering/utils/renderAI.ts
@@ -1,24 +1,4 @@
 import Ai from "../../constants/Ai";
-/*
-var aiSprites = {};
-function renderAI(obj, ctxt) {
-    var tmpIndx = obj.index;
-    var tmpSprite = aiSprites[tmpIndx];
-    if (!tmpSprite) {
-        var tmpImg = new Image();
-        tmpImg.onload = function() {
-            this.isLoaded = true;
-            this.onload = null;
-        };
-        tmpImg.src = ".././img/animals/" + obj.src + ".png";
-        tmpSprite = tmpImg;
-        aiSprites[tmpIndx] = tmpSprite;
-    }
-    if (tmpSprite.isLoaded) {
-        var tmpScale = obj.scale * 1.2 * (obj.spriteMlt||1);
-        ctxt.drawImage(tmpSprite, -tmpScale, -tmpScale, tmpScale*2, tmpScale*2);
-    }
-}*/
 
 const aiSprites: Record<number, HTMLImageElement> = {};
 
